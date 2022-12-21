50581cef58b5e0644cc565671209a332fe2c1e41
SincereCSL <SincereCSL@163.com>
2022-12-21 15:58:43 +0800

    update

--- my-app/src/main.js
+++
@@ -1,9 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-
-import router from './router/index'
-
-createApp(App)
-.use(store)
-.use(router)
-.mount('#app')
--- my-app/src/pages/about.vue
+++
@@ -1,4 +0,0 @@
-
-<template>
-    <h1>这是关于页面</h1>
-</template>
