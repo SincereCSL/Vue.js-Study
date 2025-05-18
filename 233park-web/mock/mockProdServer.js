c10517a2cffe1d2551a2b86606073914588de794
SincereCSL <SincereCSL@163.com>
2025-05-18 19:58:10 +0800

    修复mock引入方式，兼容新版Node包导出机制

--- 233park-web/mock/mockProdServer.js
+++ 233park-web/mock/mockProdServer.js
@@ -1,4 +1,4 @@
-import { createProdMockServer } from 'vite-plugin-mock/es/createProdMockServer'
+import { createProdMockServer } from 'vite-plugin-mock'
 import discoveryMock from './discovery'
 
 export function setupProdMockServer() {
