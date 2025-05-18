fc0b2af3940001f5510fda525bcb3f506c767e2f
SincereCSL <SincereCSL@163.com>
2025-05-18 18:49:49 +0800

    更新GitHub Pages部署配置

---
+++ vite.config.js
@@ -0,0 +1,44 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import { viteMockServe } from 'vite-plugin-mock'
+import path from 'path'
+import { fileURLToPath } from 'url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: '/Vue.js-Study/233park-web/',
+  plugins: [
+    vue(),
+    viteMockServe({
+      mockPath: 'mock',
+      supportTs: false,
+      localEnabled: true,
+      logger: true,
+      cors: true,
+      ignoreFiles: [],
+      watch: true,
+      esModule: true,
+      prodEnabled: true,
+      injectCode: `
+        import { setupProdMockServer } from './mock/mockProdServer';
+        setupProdMockServer();
+      `,
+    }),
+  ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  server: {
+    host: '0.0.0.0'
+  },
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    emptyOutDir: true
+  }
+}) 
